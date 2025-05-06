7047293e44eaa867157b22e1afc1e7fed08e6138
Ryan Bronwyn <rcbronwyn@gmail.com>
2025-05-06 23:12:43 +0000

    Added BubbleSort function with iterator.

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -12,11 +12,33 @@
 
 function bubbleSort(nums) {
   // code goes here
+  let swapped = true;
+  let iteration = 1
+
+  // Run an iteration as long as something was swapped;
+  while(swapped){
+    // Set swapped to false before looping through the array.
+    swapped = false;
+    let iteration = 1;
+    for(let i = 0; i < nums.length - iteration; i++){
+      // Check if the item in the current index has a greater value than the next index.
+      // If it does, swap the two values and set swapped to true.  
+      if(nums[i] > nums[i+1]){
+        let temp = nums[i];
+        nums[i] = nums[i+1];
+        nums[i+1] = temp;
+        swapped = true;
+      }
+    }
+    //Increase iteration by 1 at the end of the cycle;
+    iteration++
+  }
+  return nums;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
+test("bubble sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const sortedNums = bubbleSort(nums);
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
